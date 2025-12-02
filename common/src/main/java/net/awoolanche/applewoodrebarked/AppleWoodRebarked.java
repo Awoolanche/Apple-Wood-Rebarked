a20bf8959b1b42f2d1e1e2a7a4c353511d98e82f
Awoolanche <ZielonyXXD@wp.pl>
2025-12-02 20:54:54 +0100

    Initial commit

---
+++ common/src/main/java/net/awoolanche/applewoodrebarked/AppleWoodRebarked.java
@@ -0,0 +1,15 @@
+package net.awoolanche.applewoodrebarked;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class AppleWoodRebarked {
+    public static final String MOD_ID = "applewoodrebarked";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static void init() {
+        // Write common init code here.
+
+        LOGGER.info("[Let's Do Add-on] Apple Wood Rebarked initialized!");
+    }
+}
---
+++ neoforge/src/main/java/net/awoolanche/applewoodrebarked/neoforge/AppleWoodRebarkedNeoForge.java
@@ -0,0 +1,13 @@
+package net.awoolanche.applewoodrebarked.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import net.awoolanche.applewoodrebarked.AppleWoodRebarked;
+
+@Mod(AppleWoodRebarked.MOD_ID)
+public final class AppleWoodRebarkedNeoForge {
+    public AppleWoodRebarkedNeoForge() {
+        // Run our common setup.
+        AppleWoodRebarked.init();
+    }
+}
