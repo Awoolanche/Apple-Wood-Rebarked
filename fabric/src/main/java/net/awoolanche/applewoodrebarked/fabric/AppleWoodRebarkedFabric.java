5f41b91a95bf37fb7f1c4f405043f2a1180d270b
Awoolanche <76163840+Awoolanche@users.noreply.github.com>
2026-07-19 09:16:36 +0200

    Fixed texture swapping with built-in resource pack

--- fabric/src/main/java/net/awoolanche/applewoodrebarked/fabric/AppleWoodRebarkedFabric.java
+++ fabric/src/main/java/net/awoolanche/applewoodrebarked/fabric/AppleWoodRebarkedFabric.java
@@ -2,18 +2,31 @@ package net.awoolanche.applewoodrebarked.fabric;
 
 import net.awoolanche.applewoodrebarked.AppleWoodRebarked;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.resources.ResourceLocation;
 
+import java.util.Optional;
 
 public final class AppleWoodRebarkedFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         try {
-            Class.forName("net.awoolanche.applewoodrebarked.blocks.ModBlocks"); }
-        catch (ClassNotFoundException e) {
+            Class.forName("net.awoolanche.applewoodrebarked.blocks.ModBlocks");
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
 
         AppleWoodRebarked.init();
         AppleWoodRebarked.commonSetup();
+
+        Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(AppleWoodRebarked.MOD_ID);
+        modContainer.ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(
+                ResourceLocation.fromNamespaceAndPath(AppleWoodRebarked.MOD_ID, "resourcepacks/vinery_overrides"),
+                container,
+                ResourcePackActivationType.ALWAYS_ENABLED
+        ));
     }
 }
--- neoforge/src/main/java/net/awoolanche/applewoodrebarked/neoforge/AppleWoodRebarkedNeoForge.java
+++ neoforge/src/main/java/net/awoolanche/applewoodrebarked/neoforge/AppleWoodRebarkedNeoForge.java
@@ -16,16 +16,27 @@
     import net.minecraft.client.renderer.blockentity.HangingSignRenderer;
     import net.minecraft.client.renderer.blockentity.SignRenderer;
     import net.minecraft.client.renderer.entity.ThrownItemRenderer;
+    import net.minecraft.network.chat.Component;
     import net.minecraft.resources.ResourceLocation;
+    import net.minecraft.server.packs.*;
+    import net.minecraft.server.packs.repository.Pack;
+    import net.minecraft.server.packs.repository.PackSource;
     import net.minecraft.world.level.block.state.properties.WoodType;
     import net.neoforged.api.distmarker.Dist;
+    import net.neoforged.api.distmarker.OnlyIn;
     import net.neoforged.bus.api.IEventBus;
     import net.neoforged.bus.api.SubscribeEvent;
+    import net.neoforged.fml.ModList;
     import net.neoforged.fml.common.EventBusSubscriber;
     import net.neoforged.fml.common.Mod;
     import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
     import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
     import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+    import net.neoforged.neoforge.event.AddPackFindersEvent;
+    import org.jetbrains.annotations.NotNull;
+
+    import java.nio.file.Path;
+    import java.util.Optional;
 
 
     @Mod(AppleWoodRebarked.MOD_ID)
@@ -54,7 +65,6 @@
             });
         }
 
-
         @EventBusSubscriber(modid = AppleWoodRebarked.MOD_ID, value = Dist.CLIENT)
         public static class ClientEvents {
 
@@ -70,6 +80,48 @@
                 for (AppleBoatEntity.Type type : AppleBoatEntity.Type.values()) {
                     event.registerLayerDefinition(new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(AppleWoodRebarked.MOD_ID, type.getModelLocation()), "main"), BoatModel::createBodyModel);
                     event.registerLayerDefinition(new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(AppleWoodRebarked.MOD_ID, type.getChestModelLocation()), "main"), ChestBoatModel::createBodyModel);
+                }
+            }
+
+            @OnlyIn(Dist.CLIENT)
+            @SubscribeEvent
+            public static void onAddPackFinders(AddPackFindersEvent event) {
+                if (event.getPackType() == PackType.CLIENT_RESOURCES) {
+                    Path packPath = ModList.get().getModFileById(AppleWoodRebarked.MOD_ID)
+                            .getFile()
+                            .findResource("resourcepacks/vinery_overrides");
+
+                    event.addRepositorySource(consumer -> {
+                        PackLocationInfo packLocationInfo = new PackLocationInfo(
+                                ResourceLocation.fromNamespaceAndPath(AppleWoodRebarked.MOD_ID, "resourcepacks/vinery_overrides").toString(),
+                                Component.literal("Vinery Overrides"),
+                                PackSource.BUILT_IN,
+                                Optional.empty()
+                        );
+
+                        Pack.ResourcesSupplier resourcesSupplier = new Pack.ResourcesSupplier() {
+                            @Override
+                            public @NotNull PathPackResources openPrimary(PackLocationInfo info) {
+                                return new PathPackResources(info, packPath);
+                            }
+
+                            @Override
+                            public @NotNull PackResources openFull(PackLocationInfo info, Pack.Metadata metadata) {
+                                return new PathPackResources(info, packPath);
+                            }
+                        };
+
+                        Pack pack = Pack.readMetaAndCreate(
+                                packLocationInfo,
+                                resourcesSupplier,
+                                PackType.CLIENT_RESOURCES,
+                                new PackSelectionConfig(true, Pack.Position.TOP, false)
+                        );
+
+                        if (pack != null) {
+                            consumer.accept(pack);
+                        }
+                    });
                 }
             }
         }
